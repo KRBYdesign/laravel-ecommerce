8ef78a050beddfef3b647f6452323fb3b7184b77
Kyle Beasley <kyle.beasley17@gmail.com>
2024-10-29 16:02:05 -0500

    drop downs hidden. animation to reveal them is working

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
 const sidebar = document.querySelector('nav');
 const sidebarToggle = document.getElementById('menu-icon');
+const menExpand = document.getElementById('men-expand');
+const menMenu = document.getElementById('men-menu')
+const womenExpand = document.getElementById('women-expand');
+const womenMenu = document.getElementById('women-menu')
 
 sidebarToggle.addEventListener('click', toggleSidebar);
+menExpand.addEventListener('click', () => {
+    toggleSidebarMenu('men')
+});
+womenExpand.addEventListener('click', () => {
+    toggleSidebarMenu('women')
+});
 
 function toggleSidebar() {
     let status = sidebarToggle.ariaSelected;
 
-    console.log(status);
+    // console.log(status);
 
     if (status === 'true') {
         // close the sidebar
@@ -17,5 +27,34 @@ function toggleSidebar() {
         // open the sidebar
         sidebar.ariaSelected = 'true';
         sidebarToggle.ariaSelected = 'true';
+    }
+}
+
+function toggleSidebarMenu(menu) {
+    let menuElement;
+    let menuToggle;
+
+    if (menu === 'men') {
+        menuElement = menMenu;
+        menuToggle = menExpand;
+    }
+    else if (menu === 'women') {
+        menuElement = womenMenu;
+        menuToggle = womenExpand;
+    }
+
+    // what is the status of the menu
+    let status = menuElement.ariaSelected;
+
+    // console.log(`Toggling ${menuElement.id} and ${menuToggle.id}`)
+
+    if (status === 'true') {
+        menuElement.ariaSelected = 'false';
+        menuToggle.classList.replace('fa-caret-down', 'fa-caret-up')
+        // console.log('To status false')
+    } else {
+        menuElement.ariaSelected = 'true';
+        menuToggle.classList.replace('fa-caret-up', 'fa-caret-down')
+        // console.log('To status true')
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,9 +1,12 @@
 <img src="{{ asset('img/three-horizontal-lines-icon.svg') }}" alt="menu-icon" aria-selected="false" id="menu-icon"/>
 
 <nav aria-selected="false">
-    <a class="drop-down">Women</a>
+    <a class="drop-down">
+        <i class="fa-solid fa-caret-up" id="women-expand" aria-selected="false"></i>
+        Women
+    </a>
 
-    <div class="drop-down-menu" aria-selected="false">
+    <div class="drop-down-menu" aria-selected="false" id="women-menu">
         <a class="menu-item">Shirts</a>
         <a class="menu-item">Jackets / Sweaters</a>
         <a class="menu-item">Shorts</a>
@@ -11,9 +14,12 @@
         <a class="menu-item">Athletic</a>
     </div>
 
-    <a class="drop-down">Men</a>
+    <a class="drop-down">
+        <i class="fa-solid fa-caret-up" id="men-expand" aria-selected="false"></i>
+        Men
+    </a>
 
-    <div class="drop-down-menu" aria-selected="false">
+    <div class="drop-down-menu" aria-selected="false" id="men-menu">
         <a class="menu-item">Shirts</a>
         <a class="menu-item">Jackets / Sweaters</a>
         <a class="menu-item">Shorts</a>
